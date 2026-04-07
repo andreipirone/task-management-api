3718d9617f8b2fff9377d2e69f4d875c901a8459
Andrei Pirone <andrei.pirone@gmail.com>
2026-04-07 19:47:20 +0300

    comments to ScopeValidator.java

--- src/main/java/com/andrei/demo/config/ScopeValidator.java
+++ src/main/java/com/andrei/demo/config/ScopeValidator.java
@@ -8,6 +8,10 @@ import java.util.*;
 
 @Component
 public class ScopeValidator {
+    //i wrote this validator because the new auth0 springboot library has a weird way of doing things for some reason.
+    // the main problem is that Auth0 permissions are not mapped to Spring's default GrantedAuthorities.
+    // therefore i can't use the @PreAuthorize annotation, nor convert the JWT I recieve from the client.
+    // so this validator checks the Auth0AuthenticationToken specifically for those permissions.
     public boolean hasRequiredScopes(Authentication authentication, String... requiredScopes) {
         if (!(authentication instanceof Auth0AuthenticationToken)) {
             return false;
